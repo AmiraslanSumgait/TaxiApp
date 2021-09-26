29f5322026c229e5fe51bacea3e8bf7e8fafcc97
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-09-26 17:02:27 +0400

    Structure is created

---
+++ TaxiApp/Command/RelayCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TaxiApp.Command
+{
+    public class RelayCommand : ICommand
+    {
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        private Action<object> _execute { get; set; }
+        private Predicate<object> _canExecute { get; set; }
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute = null)
+        {
+            if (execute == null)
+            {
+                throw new NullReferenceException("Execute is null!");
+            }
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute.Invoke(parameter);
+        }
+
+    }
+}
