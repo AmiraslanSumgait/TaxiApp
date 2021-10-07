4c15c7fa08927ae4fe1a296e4dedc8910a128c86
Kənan İdayatov <idayatovkenan7@gmail.com>
2021-10-07 23:56:00 +0400

    User class added.

--- TaxiApp/Models/User.cs
+++ TaxiApp/Models/User.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace TaxiApp.Models
 {
-    class User
+    public class User : Entity
     {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
     }
 }
