f76c6163a9aa89fcd2662d18fdb0321b956beb33
Kənan İdayatov <idayatovkenan7@gmail.com>
2021-10-18 13:37:15 +0400

    Route price logic and some icons added.

---
+++ TaxiApp/Repository/RoutePriceRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace TaxiApp.Repository
+{
+    public class RoutePriceRepository
+    {
+        public static double GetRoutePrice()
+        {
+            double RoutePrice;
+            //var json = File.ReadAllText("../../Resources/RideHistory.json");
+            var json = File.ReadAllText(@"C:\Users\user\source\repos\TaxiApp\AdminPanel\Resources\RoutePrice.json");
+            RoutePrice = JsonSerializer.Deserialize<double>(json);
+
+            return RoutePrice;
+        }
+    }
+}
